ba2970d6f6aee4d74d33e22ebcacad7aa094d585
Adityagodini <adityagodini@gmail.com>
2025-03-21 19:10:36 +0530

    ad

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center">Welcome to the food delivery App</h1>
+      <img src="kofta curry.jpeg" alt="" />
+    </div>
+  )
+}
+
+export default Home
